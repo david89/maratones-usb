589e9b374c4dea08160debb7d132e4377fd43139
Daniel Izquierdo <daniel.izquierdo@gmail.com>
2013-11-01 02:03:43 +0900

    Problema 'Inline Elephant Toast'

---
+++ 2013/inline_elephant_toast/Solution/inline.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+long C[60][60];
+long blank;
+
+long comb(int n, int k)
+{
+    if(k==0)
+        return 1;
+    if(n==k)
+        return 1;
+        
+    if(C[n][k]==blank)
+        C[n][k] = comb(n-1,k-1) + comb(n-1,k);
+    
+    return C[n][k];
+}
+
+int main(int argc, char** argv)
+{
+    int N;
+
+    memset(C,0x1f,sizeof(C));
+    blank=C[0][0];
+
+    while(true)
+    {
+        cin>>N;
+
+        if(N==0)
+            break;
+
+        cout<<comb(N,N/2)/(N/2+1)<<endl;
+    }
+
+    return 0;
+}
